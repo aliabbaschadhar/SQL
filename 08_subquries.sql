a734c5f6bb1478977e69e738c3cf3263a80a79b6
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2024-11-10 18:29:08 +0500

    add comments in the files to understand what is happening in the code

--- 08_subquries.sql
+++ 08_subquries.sql
@@ -1,13 +1,23 @@
+-- Use the company database for the subsequent queries
 use company;
 
+-- Retrieve all columns from the employee table
 SELECT * FROM company.employee;
 
--- subquery in where clause
+-- Subquery in WHERE clause:
+-- A subquery is a query nested inside another query. It is used to return data that will be used in the main query.
 
---min_salary
+-- min_salary:
+-- The MIN() function is used to return the smallest value in a set of values.
+
+-- Retrieve the minimum salary from the employee table
 SELECT MIN(salary) FROM employee;
 
---name/names of employee/employees who has min salary
+-- name/names of employee/employees who has min salary:
+-- This query retrieves the name(s) of employee(s) who have the minimum salary.
+
+-- The subquery (SELECT MIN(salary) FROM employee) is used to get the minimum salary.
+-- The main query then retrieves the name(s) of employee(s) who have this minimum salary.
 SELECT name
 FROM employee
 WHERE
@@ -16,7 +26,11 @@ WHERE
         FROM employee
     );
 
---Has salary greater than min salary
+-- Has salary greater than min salary:
+-- This query retrieves the name(s) and salary(s) of employee(s) who have a salary greater than the minimum salary.
+
+-- The subquery (SELECT MIN(salary) FROM employee) is used to get the minimum salary.
+-- The main query then retrieves the name(s) and salary(s) of employee(s) who have a salary greater than this minimum salary.
 SELECT name, salary
 from employee
 WHERE
--- 09_nth_highORLow_salary.sql
+++ 09_nth_highORLow_salary.sql
@@ -1,26 +1,33 @@
 use company;
 
-SELECT name, salary from employee ORDER BY salary DESC;
+SELECT name, salary FROM employee ORDER BY salary DESC;
 
---Find the highest salary
+-- Find the highest salary
+-- This query uses a subquery to get the maximum salary and then selects employees with that exact salary
 SELECT name, salary
-from employee
-where
+FROM employee
+WHERE
     salary = (
         SELECT MAX(salary)
-        from employee
+        FROM employee
     );
 
-SELECT salary from employee ORDER BY salary DESC LIMIT 1;
+-- Get the highest salary
+-- This query simply returns the top salary value
+SELECT salary FROM employee ORDER BY salary DESC LIMIT 1;
 
---Find the 2nd highest salary
-SELECT name, salary from employee ORDER BY salary DESC LIMIT 1, 1;
+-- Find the second-highest salary
+-- This query orders salaries descending and limits to the second row
+SELECT name, salary FROM employee ORDER BY salary DESC LIMIT 1, 1;
 
---Find third highest
-SELECT name, salary from employee ORDER BY salary DESC LIMIT 4, 1;
+-- Find the third-highest salary
+-- This query orders salaries descending and limits to the fourth row (third after the first three highest)
+SELECT name, salary FROM employee ORDER BY salary DESC LIMIT 4, 1;
 
---Find the lowest salary
-SELECT name, salary from employee ORDER BY salary ASC LIMIT 1;
+-- Find the lowest salary
+-- This query orders salaries ascending and limits to the first row
+SELECT name, salary FROM employee ORDER BY salary ASC LIMIT 1;
 
---Find the 3rd lowest salary
-SELECT name, salary from employee ORDER BY salary ASC LIMIT 2, 1;
+-- Find the third-lowest salary
+-- This query orders salaries ascending and limits to the second row (third after the first two lowest)
+SELECT name, salary FROM employee ORDER BY salary ASC LIMIT 2, 1;

38a578dd598614183e94065ea6dcd55618f31d9b
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2024-11-10 22:54:28 +0500

    some changes in the employee_self added

--- 02_PracticleQuestion/03_solution.sql
+++ 02_PracticleQuestion/03_solution.sql
@@ -3,6 +3,20 @@ use company_self;
 
 SELECT * FROM employee;
 
+ALTER TABLE employee ADD COLUMN salary INT;
+
+UPDATE employee SET salary = 5000 WHERE e_id = 1;
+
+UPDATE employee SET salary = 6000 WHERE e_id = 2;
+
+UPDATE employee SET salary = 7000 WHERE e_id = 3;
+
+UPDATE employee SET salary = 8000 WHERE e_id = 4;
+
+UPDATE employee SET salary = 9000 WHERE e_id = 5;
+
+UPDATE employee SET salary = 10000 WHERE e_id = 6;
+
 SELECT *
 from employee
 WHERE
